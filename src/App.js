65e124358cf5d77cb9df2be5e9a5b15f68621690
SNarlo <sebastian.narloch82@gmail.com>
2021-07-23 17:38:12 +1000

    Started creating sidebar menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import './Sidebar/Sidebar.jsx'
+import Sidebar from './Sidebar/Sidebar.jsx';
 
 const App = () => {
   return (
     <div className="App">
-
+      < Sidebar />
     </div>
   );
 }
---
+++ src/Sidebar/Sidebar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Sidebar.css'
+import NGU_Logo from '../Media/ngu-real-estate-logo.png'
+import Leaderboard_Icon from '../Media/SVG/leaderboard.svg'
+import Branches_Icon from '../Media/SVG/branches.svg'
+import Agents_Icon from '../Media/SVG/agents.svg'
+import RELB_Logo from '../Media/relb-logo-sidebar.png'
+
+const Sidebar = () => {
+
+    return (
+        <div className="sidebar">
+            <div className='ngu-logo-section'>
+                <img src={NGU_Logo} alt='Ngu Real Estate logo' id='ngu-logo-sidebar'></img>
+            </div>
+            <div className='sidebar-menus'>
+                <div className='sidebar-menu-item'>
+                    <img class='sidebar-icon' src={Leaderboard_Icon}></img>
+                    <a href="">Leaderboards</a>
+                </div>
+                <div className='sidebar-menu-item'>
+                    <img class='sidebar-icon' src={Branches_Icon}></img>
+                    <a href="">Branches</a>
+                </div>
+                <div className='sidebar-menu-item'>
+                    <img class='sidebar-icon' src={Agents_Icon}></img>
+                    <a href="">Agents</a>
+                </div>
+            </div>
+            
+            <img id='relb-logo' src={RELB_Logo} alt='RELB logo'></img>
+        </div>
+    )
+}
+
+
+export default Sidebar
