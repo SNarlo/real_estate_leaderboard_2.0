da7d5c30d2d0c4565be223cb153af13c14b8e4b0
SNarlo <sebastian.narloch82@gmail.com>
2021-07-27 17:09:01 +1000

    Started styling leader cards, need to finish

--- src/Components/Leaderboard/Leaderboard.jsx
+++ src/Components/Leaderboard/Leaderboard.jsx
@@ -1,4 +1,5 @@
 import './Leaderboard.css'
+import John from '../../Media/Images/John-Stevenson.png'
 
 const LeaderCard = (props) => {
     return (
@@ -8,7 +9,7 @@ const LeaderCard = (props) => {
                 <span class='position'>{props.number}</span>
                 <img src={props.profile_img}></img>
                 <h2 id='agent-name'>{props.agent_name}</h2>
-                <h3 id='agent-branch'>{props.branch}</h3>
+                <h3 id='agent-branch'>{props.branch_name}</h3>
                 <h2 id='sales-total'>{props.sales_total_figure}</h2>
                 <p id='total-sales'>{props.total_sales}</p>
             </div>
@@ -26,7 +27,14 @@ const Leaderboard = (props) => {
                 <h1 id='leaderboard-title'>Sales Leaderboard</h1>
                 <div id='leader-cards-container'>
                     <LeaderCard position='second' number='2' />
-                    <LeaderCard position='first' number='1'/>
+                    <LeaderCard 
+                    profile_img={John}
+                    position='first' 
+                    number='1' 
+                    agent_name='John Stevenson' 
+                    branch_name='Toowong' 
+                    sales_total_figure='$18,760,980' 
+                    total_sales='16' />
                     <LeaderCard position='third' number='3'/>
                 </div>
             </section>   
